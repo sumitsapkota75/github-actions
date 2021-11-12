a6bb4d62e84bed0d2a62b51ee186e1d9dce2de49
Sumit <sumitsapkota0@gmail.com>
2021-11-12 15:18:46 +0545

    testing the super linter

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ func main() {
 
 	http.HandleFunc("/health-check", helloHandler)
 
-	log.Println("Listning for requests at http://localhost:8000/health-check")
+log.Println("Listning for requests at http://localhost:8000/health-check")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func helloHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Tada, 👻 Server is up and running!\n")
+io.WriteString(w, "Tada, 👻 Server is up and running!\n")
 }
