25b33117b4e897e0f508ba806a3b012630f63f35
Sumit <sumitsapkota0@gmail.com>
2021-11-12 14:50:38 +0545

    fixed routes

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 
 	http.HandleFunc("/health-check", helloHandler)
 
-	log.Println("Listning for requests at http://localhost:8000/hello")
+	log.Println("Listning for requests at http://localhost:8000/health-check")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
