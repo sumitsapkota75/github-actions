f36b02f3792d66a752ba77e5e72f5aceeeb537bf
Sumit <sumitsapkota0@gmail.com>
2021-11-12 15:42:06 +0545

    run linter test

--- main.go
+++ main.go
@@ -8,10 +8,15 @@ import (
 
 func main() {
 	http.HandleFunc("/health-check", helloHandler)
+	
 	log.Println("Listning for requests at http://localhost:8000/health-check")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
+
+
+
 func helloHandler(w http.ResponseWriter, req *http.Request) {
+
 	io.WriteString(w, "Tada, 👻 Server is up and running!\n")
 }
